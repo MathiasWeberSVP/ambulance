a10da64e7d6d577dc32256e21fe35246146de6c0
Mathias Weber <76452110+MathiasWeberSVP@users.noreply.github.com>
2026-09-22 20:26:28 +0200

    Nouveaux fichiers et renomage anciens

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'cpr-assist-v1.2';
-const FILES = ['./cpr-tool.html', './manifest.json'];
+const FILES = ['./cpr-adult.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
